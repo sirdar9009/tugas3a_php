f4ba6f420e8c748c3c1bb68f49cb0f64501bf2c3
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-10-07 12:23:14 +0700

    Add files via upload

---
+++ tugas3a.php
@@ -0,0 +1,9 @@
+<?php
+$jarak=360;
+$kecepatan=90;
+$waktu = $jarak/$kecepatan;
+
+echo "Jarak antara kota A – Z 360 km. Jika ditempuh dengan sepeda motor berkecepatan 90 km/jam maka lama perjalanan…? <br/>";
+echo "v = s/t<br/>";
+echo "v = ",$jarak ,"/",$kecepatan," = ", $waktu," Jam";
+?>
